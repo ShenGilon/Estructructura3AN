80a95a89e3b76f03d5edd6a8f7dd55a3e153aa48
Johan Steward Galeano Torres <44073891+JStewardGT@users.noreply.github.com>
2019-02-23 17:54:58 -0500

    Taller 2 punto 1

---
+++ src/Taller2/Taller2Punto1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Taller2;
+
+/**
+ *
+ * @author steward & ShenGilon
+ */
+public class Taller2Punto1 {
+
+  public static void main(String[] args) {
+        for (int a = 150; a >= 10; a -= 10) {
+
+            if (a == 140 || a == 120 || a == 80 || a == 20) a -= 10;
+            
+            if (a == 60) a -= 20;
+            
+            System.out.print(a + " ");
+            
+            if (a > 10) System.out.print("- ");
+        }
+    }
+    
+}
