bdd1658406a248d8ee605337f4e175f82b7f7f58
Johan Steward Galeano Torres <44073891+JStewardGT@users.noreply.github.com>
2019-02-23 18:05:14 -0500

    Create Taller2Punto3.java

---
+++ src/Taller2/Taller2Punto3.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Taller2;
+
+/**
+ *
+ * @author steward & ShenGilon
+ */
+public class Taller2Punto3 {
+
+    public static void main(String[] args) {
+
+        int sueldo, antiguedad, utilidad = 0;
+
+        System.out.println("Cual es el sueldo del empleado");
+        sueldo = leer.datoInt();
+
+        System.out.println("Cual es la antiguedad en años (si es menos de un año, digite 0)");
+        antiguedad = leer.datoInt();
+
+        switch (antiguedad) {
+            case 0:
+                utilidad = (int) (sueldo * 0.03);
+                break;
+            case 1:
+                utilidad = (int) (sueldo * 0.03);
+                break;
+            case 2:
+                utilidad = (int) (sueldo * 0.12);
+                break;
+            case 3:
+                utilidad = (int) (sueldo * 0.12);
+                break;
+            case 4:
+                utilidad = (int) (sueldo * 0.12);
+                break;
+            case 5:
+                utilidad = (int) (sueldo * 0.18);
+                break;
+            case 6:
+                utilidad = (int) (sueldo * 0.18);
+                break;
+            case 7:
+                utilidad = (int) (sueldo * 0.18);
+                break;
+            case 8:
+                utilidad = (int) (sueldo * 0.18);
+                break;
+            case 9:
+                utilidad = (int) (sueldo * 0.18);
+                break;
+            case 10:
+                utilidad = (int) (sueldo * 0.25);
+                break;
+            default:
+                utilidad = (int) (sueldo * 0.25);
+                break;
+        }
+
+        System.out.println("La utilidad es " + utilidad);
+
+    }
+}
