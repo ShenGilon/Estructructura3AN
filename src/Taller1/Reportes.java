26c98485ca21e8f7ecc82da4350dbe9e4ed5cd48
Johan Steward Galeano Torres <44073891+JStewardGT@users.noreply.github.com>
2019-02-23 17:22:03 -0500

    Cambio en tipo de dato

--- src/Taller1/Reportes.java
+++ src/Taller1/Reportes.java
@@ -22,18 +22,18 @@ public class Reportes {
         obj1.Nombre = leer.dato();
         
           System.out.println("Ingrese su CC: ");
-        obj1.cc= leer.datoInt();
+        obj1.cc= (int) leer.datoDouble();
 
         
  
         System.out.println("Ingrese su celular: ");
-        obj1.setCelular(leer.datoInt());
+        obj1.setCelular((long) leer.datoDouble());
 
          System.out.println("Ingrese su cuenta: ");
-        obj1.setCuenta(leer.datoInt());
+        obj1.setCuenta((long) leer.datoDouble());
 
         System.out.println("Ingrese su sueldo: ");
-        obj1.setSueldo(leer.datoInt());
+        obj1.setSueldo((int) leer.datoDouble());
 
         System.out.println("");
         System.out.println("");
