dab0e6362e900435c40dff560a36d2b69e878312
ShenGilon <Shenckeli.ordonezg@ecci.edu.co>
2019-02-28 00:48:58 -0500

    Punto2_4

--- src/Taller4/Taller4Punto2.java
+++ src/Taller4/Taller4Punto2.java
@@ -1,21 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Taller4;
 
-/**
- *
- * @author stewa
- */
+import java.util.Arrays;
+
 public class Taller4Punto2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int contPos = 0, contNeg = 0, j = 0, k=0;
+        int numeros[] = new int[6];
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Ingrese el numero para la posicion " + i + " = ");
+            numeros[i] = leer.datoInt();
+            System.out.println("");
+
+            if (numeros[i] > 0) {
+                contPos++;
+            } else {
+                contNeg++;
+            }
+        }
+
+        int[] positivos = new int[contPos];
+        int[] negativos = new int[contNeg];
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > 0) {
+                positivos[j] = numeros[i];
+                j++;
+            } else {
+                negativos[k] = numeros[i];
+                k++;
+            }
+        }
+
+        Arrays.sort(positivos);
+        Arrays.sort(negativos);
+
+        System.out.println("Numeros Positivos");
+        j=0;
+        for (int i = positivos.length; i > 0; i--) {
+            System.out.print(positivos[(i - 1)] + "\t");
+            j++;
+        }
+            System.out.println("");
+        
+        System.out.println("Numeros negativos");
+        for (int i = 0; i < negativos.length; i++) {
+            System.out.print(negativos[i] + "\t");
+        }
     }
-    
 }
